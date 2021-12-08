96c6c89413594d74540e9620398e740f66e32b2e
Michael Smyth <michael.m.smyth@gmail.com>
2021-12-08 16:41:22 +0000

    ok

--- src/Todo.test.js
+++ src/Todo.test.js
@@ -1,14 +1,3 @@
 import {render, screen} from '@testing-library/react';
 import Todo from './Todo';
 
-const mockData = [
-    'test1',
-    'test2',
-]
-
-
-describe("todo tests", () => {
-    test('renders the heading', () => {
-        render(<Todo todos={mockData}/>>)
-    })
-})
